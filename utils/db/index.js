788834c8c163759a174af98349ea98b7b926036e
jake tribe <jtribe32@gmail.com>
2022-04-19 10:49:31 -0600

    Update index.js

--- utils/db/index.js
+++ utils/db/index.js
@@ -4,18 +4,11 @@ import serviceAccount from "./serviceAccountKey.json";
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(
-        process.env.NODE_ENV === "production"
-          ? {
-              projectId: process.env.FIREBASE_PROJECT_ID,
-              clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-              privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(
-                /\\n/g,
-                "\n"
-              ),
-            }
-          : serviceAccount
-      ),
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      }),
     });
   } catch (error) {
     console.log("Firebase admin initialization error", error.stack);
